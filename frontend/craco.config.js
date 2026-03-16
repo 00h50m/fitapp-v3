0fab6c463be705aa73052841b3eb4b0a488bb55f
Giovanna <giovanna.ncpupo@gmail.com>
2026-03-16 18:03:45 +0000

    fix: craco config com TSC_COMPILE_ON_ERROR no processo

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,18 +1,17 @@
 const path = require('path');
 
+process.env.TSC_COMPILE_ON_ERROR = 'true';
+process.env.ESLINT_NO_DEV_ERRORS = 'true';
+
 module.exports = {
   webpack: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
     configure: (webpackConfig) => {
-      // Remove ForkTsChecker
       webpackConfig.plugins = webpackConfig.plugins.filter(
-        (plugin) => plugin.constructor.name !== 'ForkTsCheckerWebpackPlugin'
-      );
-      // Remove ESLint
-      webpackConfig.plugins = webpackConfig.plugins.filter(
-        (plugin) => plugin.constructor.name !== 'ESLintWebpackPlugin'
+        (plugin) => plugin.constructor.name !== 'ForkTsCheckerWebpackPlugin' &&
+                    plugin.constructor.name !== 'ESLintWebpackPlugin'
       );
       return webpackConfig;
     },
